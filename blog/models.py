6a040b8965d32a393c5b9b2ffec0fe1c321c7cd4
Mayank <makshrestha@gmail.com>
2015-09-06 10:56:24 +0545

    added blog app

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+#inheritted from django.db.models.Model
+class Blog(models.Model):
+	content = models.TextField()
+
+
---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
+
+# Create your views here.
+
+def show(request):
+	#Select * From blogs
+	blogs = Blog.objects.all()
+	resp = ''
+
+	print (blogs)
+	for blog in blogs:
+		print('='*72)
+		print(blog)
+		resp+= blog.content + '-------'
+
+	return HttpResponse(resp)
